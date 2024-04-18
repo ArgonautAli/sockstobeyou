31c1278919d9f0a61529a730c8ed5f69d9e9e963
ArgonautAli <haider.salar@gmail.com>
2024-04-18 22:01:39 +0530

    feat: socket wip

--- app/main.py
+++ app/main.py
@@ -1,15 +1,42 @@
-from fastapi import FastAPI
-from websocket import WebSocket
+from fastapi import FastAPI, WebSocketDisconnect, WebSocket as WebSocketType
+from websocket import WebSocket 
 
 app = FastAPI()
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
 
 @app.get("/")
 async def get():
     return {"socks to be": "you" }
 
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket
-    data = await websocket.receive_text()
-    await websocket.send_text(f"Message text: {data}")
+async def websocket_endpoint(websocket: WebSocketType, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"you wrote: {data}", websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"client #{client_id} has left the chat")
 
